8977b941cc6f3c32388bfe34b8aafa63f3144b2c
devel@mauriciocap.com <devel@mauriciocap.com>
2023-03-22 19:15:38 -0400

    FIX: gitpages usando HashRouter

--- FRONTEND_BICI_CLUB/src/main.jsx
+++ FRONTEND_BICI_CLUB/src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
 import Root from './routes/root';
 import Login from './components/Login/Login';
 import SignUp from './components/SignUp/SignUp';
 
-const router = createBrowserRouter([
+const router = createHashRouter([ //A: compatible desplegar en coso.com/micarpeta/ ej gitpages
   {
     path: '/',
     element: <Root />,
@@ -16,6 +16,10 @@ const router = createBrowserRouter([
     path: 'home',
     element: <HomePage />,
   },
+	{
+		path: '*',
+		element: <div>No encontre esa url, desplegaste en una subcarpeta? esta bien configurado ReactRouter? Mira main.jsx</div>,
+	},
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
