4539e11e06d0676410e937a38b51879862c332ec
EnzoDaneri <enzodaneri@hotmail.com>
2023-03-27 16:35:55 -0300

    feat(frontend): add fetch method to dispatch user state

--- FRONTEND_BICI_CLUB/src/routes/root.jsx
+++ FRONTEND_BICI_CLUB/src/routes/root.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import AddLinkTravel from '../components/AddLinkTravel/AddLinkTravel';
 import SetProfilePicture from '../components/SetProfilePicture/SetProfilePicture';
 import Login from '../components/Login/Login';
@@ -9,10 +9,20 @@ import logo_bici_club from '../images/logo-bici-club.jpg';
 import AddTravelPage from '../pages/AddTravelPage/AddTravelPage';
 import HomePage from '../pages/HomePage/HomePage';
 import store from '../redux/store/store';
-import { Provider } from 'react-redux';
+import { Provider, useDispatch } from 'react-redux';
+import { json } from 'react-router-dom';
+import { addUser } from '../redux/store/userSlice';
 
 function Root() {
   const [isMember, setIsmember] = useState(false);
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users/1') //replace by the app endpoint
+      .then((response) => response.json())
+      .then((data) => dispatch(addUser(data)))
+      .catch((error) => console.log('error: ', error));
+  }, []);
 
   return (
     <Provider store={store}>
